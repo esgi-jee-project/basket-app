4209265c79c66a73809742fa4bb6390a76c6c6cf
Ken-Williams OYE <k.williams.oye@gmail.com>
2020-04-15 01:14:08 +0200

    WIP post match

--- src/main/java/com/esgi/jee/basket/db/Match.java
+++ src/main/java/com/esgi/jee/basket/db/Match.java
@@ -3,10 +3,8 @@ package com.esgi.jee.basket.db;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.util.Date;
+import javax.persistence.*;
+
 
 @Entity
 @Getter
@@ -14,8 +12,9 @@ import java.util.Date;
 public class Match {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
-    @Column(nullable = false)
-    private Date date;
+    @Column()
+    private String date;
 }
--- src/main/java/com/esgi/jee/basket/web/MatchController.java
+++ src/main/java/com/esgi/jee/basket/web/MatchController.java
@@ -5,6 +5,7 @@ import com.esgi.jee.basket.db.MatchRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,11 @@ public class MatchController {
     @GetMapping(path = "/matchs")
     public List<Match> getAll() {
         return matchRepository.findAll();
+    }
+
+    @PostMapping(path = "/match")
+    public Match create(@RequestBody Match match){
+
+        return matchRepository.save(match);
     }
 }
