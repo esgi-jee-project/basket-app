5aa4f58c34aef30a58e70d41e991316de2ac7c37
Mathieu <mathieu.0598@gmail.com>
2020-06-29 00:37:57 +0200

    Update test and circle ci conf

--- src/test/java/com/esgi/jee/basket/web/TeamControllerTest.java
+++ src/test/java/com/esgi/jee/basket/web/TeamControllerTest.java
@@ -25,7 +25,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
