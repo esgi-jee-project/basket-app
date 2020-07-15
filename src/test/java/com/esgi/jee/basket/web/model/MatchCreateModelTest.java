0b152f2cfa10f732cc2f5c100a5cdc7d86dc2009
Ken-Williams OYE <k.williams.oye@gmail.com>
2020-07-15 11:32:49 +0200

    remove test

--- src/test/java/com/esgi/jee/basket/web/model/MatchCreateModelTest.java
+++ src/test/java/com/esgi/jee/basket/web/model/MatchCreateModelTest.java
@@ -7,8 +7,6 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.Date;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
